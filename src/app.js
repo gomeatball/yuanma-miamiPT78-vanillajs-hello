a7a78e103799d91351e12d499f39ecd922ecec06
Yuan Ma <mayuan131522@gmail.com>
2025-03-29 14:36:29 +0000

    main

--- src/app.js
+++ src/app.js
@@ -7,17 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun = ['the', 'our', 'you', 'my', 'her', 'his'];
-  let adjective = ['great', 'big', 'silly', 'happy', 'thoughtful'];
-  let noun = ['jogger', 'racoon', 'panda', 'pig', 'chicken'];
-  let extension = [".com", '.us', '.org', '.dev', '.net']
+  let pronoun = ['the', 'our'];
+  let adjective = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ["capcom", '.us']
   
+
+let domainName = "";
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adjective.length; j++) {
     for (let z = 0; z < noun.length; z++) {
      for (let x = 0; x < extension.length; x++) {
-      console.log(`${pronoun[i]}${adj[j]}${noun[z]}${extension[x]}`);
+   
+     if (extension[x] === "capcom") {
+  let  newExtensionArray = extension[x].split("");
+      newExtensionArray.splice(3,0,".")
+      let newExtension = newExtensionArray.join("")
+            domainName = `${pronoun[i]}${adjective[j]}${noun[z]}${newExtension}`
+            console.log(domainName);
+            
+     }else {
+            domainName = `${pronoun[i]}${adjective[j]}${noun[z]}${extension[x]}`; 
+      console.log(domainName);
       
+     }
+        
+      }
      }
 
   
@@ -31,6 +46,6 @@ for (let i = 0; i < pronoun.length; i++) {
 
 
 
-};
+
 
 
